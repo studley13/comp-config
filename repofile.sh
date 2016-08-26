990a9976b64d613535988124249ae14dc63fab56
Curtis Millar <github@curtism.me>
2016-08-26 11:46:27 +1000

    Added repo stuff for web

---
+++ repofile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+reponame=$SERVER_NAME
+
+# Headers
+cat << __endHeaders
+Content-Type: text/plain; charset=utf-8
+Content-Disposition: download; filename="${reponame}.repo"
+
+__endHeaders
+
+# Repofile
+cat << __endRepo
+[$reponame]
+name=Curtis's Repository
+gpgcheck=0
+baseurl=http://${SERVER_NAME}/${CONTENT_PREFIX}/rpms/\$basearch/
+enabled=1
+__endRepo
+
+#env
